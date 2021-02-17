1ffe5b73bacb62d3ff20644ddc4df8930d7a7efe
markle12 <markle12@gmail.com>
2021-02-17 15:53:00 -0700

    actually send the login request

--- client/auction21-client/src/App.tsx
+++ client/auction21-client/src/App.tsx
@@ -5,14 +5,22 @@ import './App.css';
 let googleClientId = "151004553284-r57cgf5ipjipqjnvcn2ijt6aep9kbh3b.apps.googleusercontent.com"
 export default class App extends React.Component {
   onResponse = (response: any) => {
-    console.log(response);
+    var req = new XMLHttpRequest();
+    req.addEventListener("load", (event) => {
+      console.log(req.responseText);
+    });
+
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.open('POST', 'auction.troop712.org:80/api/login');
+    req.send(JSON.stringify({accessToken: response.accessToken}));
   }
+
   render() {
     return (
       <div className="App">
         <GoogleLogin
           clientId={googleClientId}
-          buttonText="Login"
+          buttonText="Login with Google"
           onSuccess={this.onResponse}
           onFailure={this.onResponse}
           cookiePolicy={'single_host_origin'}
